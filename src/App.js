3245ef83018b71e2e022c3b89c1fa0c4c322e7e9
Jennifer Lukban <lukbaj@uw.edu>
2021-02-02 04:04:44 -0800

    Add messages class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 import TextInput from './TextInput';
+import Message from './Message'
 
 function App() {
-  const [messages,setMessages] = useState([{text:'hello'}])
-  
-  return <div className="App">
-    
-    <header className="header">
-      <div className="logo" />
+  const [messages, setMessages] = useState([])
+
+  return <div className= "App">
+ 
+    <header className= "header">
+      <div className= "logo" />
       CHATTER
     </header>
 
-    <div className="message">
-      {messages[0].text}
-    </div>
+    <main className= "messages">
+      {messages.map((m,i) => {
+        return <Message key = {i} {...m} />
+      })}
+    </main>
 
     <TextInput 
-      send={(t)=> setMessages([{text:t}])}
+      send={(t)=> setMessages( [{text:t}, ...messages] )}
     />
 
   </div>
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,30 +1,34 @@
 
-import {useState} from 'react'  // importing use state
+import {useState} from 'react'  // importing useState from react
+// import { AiOutlineSend } from "react-icons/ai";
 
 function TextInput(props) {
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-    function sendMessage() {
-        if(text==='') return // skip the function
-        props.send(text)
-        setText('')
+  function sendMessage(){
+    if(text==='') return // skip the function
+    props.send(text)
+    setText('')
+  }
+  function keyPressed(e){
+    if(e.key==='Enter') {
+      sendMessage()
     }
+  }
 
-    function keyPressed(e) {
-        if(e.key==='Enter') {
-            sendMessage()
-        }
-    }
-
-    return <footer className="text-input">
-       <input className="input"
-          placeholder="Write your message"
-          value={text}
-          onChange={e=> setText(e.target.value)}
-          onKeyPress={keyPressed}
-       />
-       <button className="button" onClick={sendMessage} disabled={!text}/>
-    </footer>
+  return <footer className="text-input">
+    <input className="input"
+      placeholder="Write your message"
+      value={text}
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={keyPressed}
+    />
+    <button onClick={sendMessage} disabled={!text} className="button">
+      {/* <AiOutlineSend title="send" 
+        style={{minWidth:'0.7rem',marginLeft:2}} 
+      /> */}
+    </button>
+  </footer>
 }
 
 export default TextInput

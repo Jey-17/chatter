1acc56d1c9c5636aa80a562dd2db196af77f7ddb
Jennifer Lukban <lukbaj@uw.edu>
2021-02-08 23:49:32 -0800

    Trial 1 on boom attempt

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import './App.css';
 import TextInput from './TextInput';
 import Message from './Message'
 import NamePicker from './NamePicker'
+import {db, useDB} from './db'
 
 function App() {
-  const [messages, setMessages] = useState([])
+  const messages = useDB()
+  const [username,setUsername] = useState(
+    localStorage.getItem('username') || ''
+  )
 
-  return <div className= "App">
+  console.log(messages)
+  return <div className="App">
  
-    <header className= "header">
-      <div className= "logo" />
+    <header className="header">
+      <div className="logo" />
       FRUITY CHATTER
-      <NamePicker/>
+      <NamePicker saveName={setUsername} />
     </header>
 
-    <main className= "messages">
-      {messages.map((m,i) => {
-        return <Message key = {i} {...m} />
+    <main className="messages">
+      {messages.map((msg,i)=> {
+        const isMe = msg.name===username
+        return <Message key={i} {...msg} isMe={isMe} />
       })}
     </main>
 
     <TextInput 
-      send={(t)=> setMessages( [{text:t}, ...messages] )}
+      send={(t)=> db.send({text:t, name:username, date:new Date()})}
     />
+    
+    {/* <TextInput 
+      send={(t)=> db.send({text:t, name:username, date:new Date()})}
+    /> */}
 
   </div>
 }
--- src/NamePicker.js
+++ src/NamePicker.js
@@ -1,25 +1,29 @@
 import {useState} from 'react'
 
-
-function NamePicker() {
+function NamePicker(props){
     const [showInput, setShowInput] = useState(false)
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState(
+        localStorage.getItem('username') || ''
+    )
 
+    function save(){
+        props.saveName(username)
+        setShowInput(false)   
+        localStorage.setItem('username',username)     
+    }
     if (showInput) {
-        return <div className = "name-picker">
+        return <div className="name-picker">
             <input value={username}
                 onChange={e=> setUsername(e.target.value)}
             />
-            <button onClick={()=> setShowInput(false)}>
-                OK 
-            </button>
+            <button onClick={save}>OK</button>
         </div>
     }
 
-    return <div className = "name-picker">
-        <div> {username} </div>
-        <button onClick={() => setShowInput(true)}>
-            EDIT    
+    return <div className="name-picker">
+        <div>{username}</div>
+        <button onClick={()=> setShowInput(true)}>
+            EDIT
         </button>
     </div>
 }
